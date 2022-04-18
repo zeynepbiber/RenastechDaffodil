5014ba7a1c540c8abe7436f31277f0adf5e9b26f
Thengin <engin0zengin@gmail.com>
2022-04-17 21:46:12 -0400

    first commit

---
+++ 02_JAVA/src/student/engin/lab01.java
@@ -0,0 +1,9 @@
+package student.engin;
+
+public class lab01 {
+    public static void main(String[] args) {
+        //Write a return method that can reverse  String
+        //Ex: Reverse("ABCD"); ==> DCBA
+
+    }
+}
