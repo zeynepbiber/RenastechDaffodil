12516d12e47a838107f8800881ba5191327c0602
Sanjay <b.co@c.com>
2022-04-26 12:43:49 -0400

    labs

--- 02_JAVA/src/student/Sanjay_HR/Lab07.java
+++ 02_JAVA/src/student/Sanjay_HR/Lab07.java
@@ -2,4 +2,10 @@ package student.Sanjay_HR;
 
 public class Lab07 {
     //Write a return method that can concate two arrays
+    public static void main(String[] args) {
+
+    }
+//    public static void concateArrays(int[] a, int[] b){
+//        int [] concated=
+//    }
 }
--- 02_JAVA/src/student/Sanjay_HR/Lab10.java
+++ 02_JAVA/src/student/Sanjay_HR/Lab10.java
@@ -5,4 +5,18 @@ public class Lab10 {
     // print "FIN" instead of the number and for numbers which are a multiple of 5,
     // print "RA" instead of the number. for numbers which are a multiple of both 3 and 5,
     // print "FINRA" instead of the number.
+    public static void main(String[] args) {
+        finRA();
+    }
+    public static void finRA(){
+        for (int i = 1; i <=30 ; i++) {
+            if(i%3==0){
+                System.out.println("FIN: "+i);
+            }if(i%5==0){
+                System.out.println("RA: "+i);
+            }if(i%3==0 && i%5==0){
+                System.out.println("FINRA: "+i);
+            }
+        }
+    }
 }
