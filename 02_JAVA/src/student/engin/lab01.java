0d5df70c7bd6438ab696581858acbf433d499b93
Thengin <engin0zengin@gmail.com>
2022-04-27 16:06:37 -0400

    lab01

--- 02_JAVA/src/student/engin/lab01.java
+++ 02_JAVA/src/student/engin/lab01.java
@@ -4,6 +4,7 @@ public class lab01 {
     public static void main(String[] args) {
         //Write a return method that can reverse  String
         //Ex: Reverse("ABCD"); ==> DCBA
+        System.out.println("ss");
 
     }
 }
