5183b5e9de3b046100c8e9862442ccac4e7cc033
Sanjay <b.co@c.com>
2022-04-27 17:13:29 -0400

    labs

--- 02_JAVA/src/student/Sanjay_HR/Lab12.java
+++ 02_JAVA/src/student/Sanjay_HR/Lab12.java
@@ -2,4 +2,14 @@ package student.Sanjay_HR;
 
 public class Lab12 {
     //Write a method which can identifies given number is even or odd
+    public static void main(String[] args) {
+        oddOrEven(30);
+    }
+    public static void oddOrEven(int a){
+    if(a%2==0){
+        System.out.println("Number is even");
+    }else{
+        System.out.println("Number is odd");
+    }
+    }
 }
