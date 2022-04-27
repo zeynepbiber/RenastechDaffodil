48f983e7ec237965fbc84513e3fa321533e9a2ac
yousefzangana <yousefzangana@yahoo.com>
2022-04-26 23:20:27 -0700

    lab

--- 02_JAVA/src/student/Ulzana/lab01.java
+++ 02_JAVA/src/student/Ulzana/lab01.java
@@ -1,22 +1,27 @@
 package student.Ulzana;
 
 public class lab01 {
+    //  ## Lab 01 - //(String) Reverse
+    //Write a return method that can reverse  String
+    // Ex: Reverse("ABCD"); ==> DCBA
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-      System.out.println(reverseString("Java"));
+        System.out.println(reverseString("Java"));
 
-  }
+    }
 
-   public static String reverseString(String word){
-       //(String) Reverse
-       //Write a return method that can reverse  String
-       // Ex: Reverse("ABCD"); ==> DCBA
+    public static String reverseString(String word){
+        //(String) Reverse
+        //Write a return method that can reverse  String
+        // Ex: Reverse("ABCD"); ==> DCBA
 
-      String result="";
-         for (int i = word.length()-1;   i >=0;   i--) {
-              result += word.charAt(i);}
+        String result="";
+        for (int i = word.length()-1;   i >=0;   i--) {
+            result += word.charAt(i);}
+        System.out.println("Entered word : "+word+ ". Reversed : ");
 
-      return result;
+        return result;
+
   }
 }
