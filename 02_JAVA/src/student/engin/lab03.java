ab2b9b7df3ce8129f07a73fdba25a7dabfde7c7e
engin <ekilic@prowaveelectric.com>
2022-04-28 11:01:16 -0400

    lab04

--- 02_JAVA/src/student/engin/lab03.java
+++ 02_JAVA/src/student/engin/lab03.java
@@ -1,6 +1,6 @@
 package student.engin;
 
-public class labo3 {
+public class lab03 {
 //Write a return method that can remove the duplicated values from String
 //Ex: removeDup("AAABBBCCC") ==> ABC
 public static void main(String[] args) {
