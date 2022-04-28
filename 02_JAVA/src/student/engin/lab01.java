5aa54087580d1a566fd09bf257060ac4bbdb0c00
engin <ekilic@prowaveelectric.com>
2022-04-28 07:58:33 -0400

    lab01+

--- 02_JAVA/src/student/engin/lab01.java
+++ 02_JAVA/src/student/engin/lab01.java
@@ -4,6 +4,12 @@ public class lab01 {
     public static void main(String[] args) {
         //Write a return method that can reverse  String
         //Ex: Reverse("ABCD"); ==> DCBA
+        
+        String str="ABCD";
+        for (int i = str.length()-1; i >=0; i--) {
+            System.out.print(str.charAt(i));
+            
+        }
 
     }
 }
