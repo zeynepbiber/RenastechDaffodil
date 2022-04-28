12bc9944d4f015b99caf1f52682260d1485db885
engin <ekilic@prowaveelectric.com>
2022-04-28 14:26:49 -0400

    lab07 updated

--- 02_JAVA/src/student/engin/lab07.java
+++ 02_JAVA/src/student/engin/lab07.java
@@ -7,11 +7,11 @@ public class lab07 {
 public static void main(String[] args) {
     int[] a={1,2,3};
     int[] b={4,5,6};
-    concate2arr(a, b);
+    System.out.println(Arrays.toString(concate2arr(a, b)) );
     
 }
 
-public static void concate2arr(int[] arr1, int[] arr2){
+public static int[] concate2arr(int[] arr1, int[] arr2){
     int[] arr3=new int[arr1.length + arr2.length];
     for (int i = 0; i < arr1.length; i++) {
         arr3[i]=arr1[i];
@@ -19,7 +19,9 @@ public static void concate2arr(int[] arr1, int[] arr2){
     for (int i = 0; i < arr2.length; i++) {
         arr3[arr1.length+i]=arr2[i];
     }
-    System.out.println(Arrays.toString(arr3));
+   // System.out.println(Arrays.toString(arr3));
+
+    return arr3;
     
 }
 
