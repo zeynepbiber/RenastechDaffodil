c2a6b50e74cd27a100ab2f070a3d6ea271940632
engin <ekilic@prowaveelectric.com>
2022-04-29 14:47:56 -0400

    09

---
+++ 02_JAVA/src/student/engin/lab09.java
@@ -0,0 +1,32 @@
+package student.engin;
+import java.util.Arrays;
+public class lab09 {
+//Write a return method that can sort an int array in descending order without using the sort method of the Arrays class
+//Ex: int[] arr = {10,20,7, 8, 90};
+//         arr = Sort(arr); ==> {90, 20, 10, 8, 7};
+
+public static void main(String[] args) {
+    int[] arr = {10,20,7,8,90};
+    System.out.println(Arrays.toString(arr));
+}
+
+public static int[] ascanding(int[] arr) {
+    boolean isSwapped= false;
+    while (!isSwapped) {
+        isSwapped=true;
+        int temp=0;
+        for(int i =0;i<arr.length;i++) {
+            if(arr[i+1]<arr.length && arr[i]<arr[i+1]) {
+                temp = arr[i+1];
+                arr[i+1] = arr[i];
+                arr[i] = temp;
+               // System.out.println(arr[i]);
+                isSwapped=false;
+            }
+        }
+    }
+    return arr;
+}
+
+
+}
