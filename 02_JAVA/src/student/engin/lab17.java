77e2fdef5aef7361f0d1af91eff504699e4f8a0f
engin <ekilic@prowaveelectric.com>
2022-05-04 15:27:03 -0400

    lab17 not completed... check it

--- 02_JAVA/src/student/engin/lab17.java
+++ 02_JAVA/src/student/engin/lab17.java
@@ -9,27 +9,28 @@ public class lab17 {
     //input:  {1,0,2,0,3,0,4,0};
     //output: [1, 2, 3, 4, 0, 0, 0, 0]
     public static void main(String[] args) {
-        int[] x={0,2,3,0,4,5,0,3};
+        int[] x={0,0,3,5,0,3};
         move_0_to_back(x);
     }
 
     public static void move_0_to_back(int[] arr){
-        ArrayList<Integer> arrL=new ArrayList<>();
         int length=1;
+        int tem=0;
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i]==0 && arr.length-length!=0 ){
-            arr[arr.length-length]=arr[i];
-            arr[i]= arr[arr.length-length];
-            length++;
-            }
-            if (arr[i]==0 && arr.length-length==0)
-                arr[arr.length-length-1]=arr[i];
-                arr[i]= arr[arr.length-length-1];
+             if (arr[i]==0 && arr.length-length!=0 ){
+                 tem=arr[arr.length-length];  
+                 arr[arr.length-length]=arr[i];  
+                 arr[i]= tem;  
+                }
+             if(arr[i]==0 && arr.length-length==0 && arr.length-length-1!=0 && length < arr.length ){
+                 tem=arr[arr.length-length-1];  //5
+                 arr[arr.length-length-1]=arr[i];  //0
+                 arr[i]= tem;  //5
+                    
+                }
                 length++;
-            }
-
+        }
         System.out.println(Arrays.toString(arr));
-
     }
 
 }
