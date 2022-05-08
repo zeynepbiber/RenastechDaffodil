96c8625cbead65e2309fb7e6df537a0ea47ef89d
Your Name <gbulgan@hotmail.com>
2022-05-08 00:14:31 -0400

    lab homework

--- 02_JAVA/src/student/gulsumB/lab22.java
+++ 02_JAVA/src/student/gulsumB/lab22.java
@@ -6,6 +6,5 @@ import static java.lang.Integer.reverse;
 
 //Write a return method that can reverse negative number and return it as int
 public class lab22 {
-        public static void main (String args[]) {
-        }
+
 }
