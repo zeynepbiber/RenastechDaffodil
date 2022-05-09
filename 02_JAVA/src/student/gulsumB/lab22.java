6c161d2ecc77a06a12e58386c5bcb4bd098dd1d1
Your Name <gbulgan@hotmail.com>
2022-05-08 20:57:38 -0400

    lab homework

--- 02_JAVA/src/student/gulsumB/lab22.java
+++ 02_JAVA/src/student/gulsumB/lab22.java
@@ -1,10 +1,16 @@
 package student.gulsumB;
 
-import java.util.Scanner;
-
-import static java.lang.Integer.reverse;
 
 //Write a return method that can reverse negative number and return it as int
 public class lab22 {
-
-}
+    public static void main(String[] args) {
+        System.out.println(reverseNum(-3265626));
+    }
+    public static int reverseNum(int  num) {
+        String str = new StringBuilder(""+num).reverse().toString();
+        if(num < 0) {
+            str = "-"+str.substring(0, str.length()-1);
+        }
+        return Integer.valueOf(str);
+    }
+        }
