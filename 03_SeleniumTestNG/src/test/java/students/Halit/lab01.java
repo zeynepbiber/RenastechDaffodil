bd0e7acf301684002c9c47119faa078edc75787e
Halit O <halit334@gmail.com>
2022-05-11 21:15:30 -0400

    halit new update

--- 03_SeleniumTestNG/src/test/java/students/Halit/lab01.java
+++ 03_SeleniumTestNG/src/test/java/students/Halit/lab01.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-
 public class lab01 {
 
     public static void main(String[] args) {
@@ -14,17 +13,19 @@ public class lab01 {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.etsy.com/");
         driver.manage().window().maximize();
-        System.out.println("Title is going to print below:");
         String title=driver.getTitle();
-        System.out.println(title);
 
         WebDriverManager.firefoxdriver().setup();
         WebDriver driver1=new FirefoxDriver();
         driver1.get("https://www.amazon.com/");
         String amazontitle=driver1.getTitle();
+        driver1.close();
+
+        System.out.println("Etsy title is going to print below:");
+        System.out.println(title);
+        System.out.println("\n*******************************************\n");
         System.out.println("Amazon title will print below:");
         System.out.println(amazontitle);
-
 
     }
 }
