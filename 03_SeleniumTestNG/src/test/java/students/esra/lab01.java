66c2ea32363c4bfea57b8001a77a12529593739a
erkanozcelik19@gmail.com <Karpuz.47>
2022-05-13 15:58:26 -0400

    MyAssigment

---
+++ 03_SeleniumTestNG/src/test/java/students/esra/lab01.java
@@ -0,0 +1,30 @@
+package students.esra;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class lab01 {
+   /* #Task 1
+
+    Go to https://www.etsy.com/
+    Maximize window
+
+##Task 2
+    Go to https://www.amazon.com/
+
+    */
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver chrome=new ChromeDriver();
+        chrome.get("https://www.etsy.com/ca/");
+        chrome.manage().window().maximize();
+
+        chrome.get("https://www.google.ca/");
+        System.out.println("chrome .getTitle"+chrome.getTitle());
+
+    }
+
+}
