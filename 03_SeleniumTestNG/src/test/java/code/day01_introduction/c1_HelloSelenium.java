9a425b37a9f5dc502183c0063ff2201ece113460
Suleyman <slymnyldz6327@gmail.com>
2022-05-17 03:33:43 -0400

    suleyman

--- 03_SeleniumTestNG/src/test/java/code/day01_introduction/c1_HelloSelenium.java
+++ 03_SeleniumTestNG/src/test/java/code/day01_introduction/c1_HelloSelenium.java
@@ -14,30 +14,22 @@ public class c1_HelloSelenium {
 
 
 
-    public class c1_HelloSelenium {
-        public static void main(String[] args) {
-            System.out.println("Hello World");
-            //set up chrome driver
-            WebDriverManager.chromedriver().setup();
+                //create a driver instance
+                WebDriver driver = new ChromeDriver();
 
-            //create a driver instance
-            WebDriver driver = new ChromeDriver();
-
-            //navigate to chrome browser
-            driver.get("https://www.etsy.com/");
+                //navigate to chrome browser
+                driver.get("https://www.etsy.com/");
 
 
-        //navigate to chrome browser
-         driver.get("https://www.etsy.com/");
+                //navigate to chrome browser
+                driver.get("https://www.etsy.com/");
 
 
-    }
+            }
 
 
 
-    }
-
-}
+        }
 
 
 
