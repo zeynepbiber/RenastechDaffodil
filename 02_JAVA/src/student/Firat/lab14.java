f7eedba02c2b38d18f80a1abde0733a2c2a8c74e
mahiralkan <mahir.alkan.100@gmail.com>
2022-05-18 10:32:37 -0600

    pr fix2

--- 02_JAVA/src/student/Firat/lab14.java
+++ 02_JAVA/src/student/Firat/lab14.java
@@ -1,52 +1,4 @@
-<<<<<<< HEAD
-//package student.Firat;
-//
-//public class lab14 {
-//    //Write a method that can return the sum of the digits in a string
-//    public static void main(String[] args) {
-//        System.out.println(sumania("jg2fghh3h"));
-//    }
-//
-//
-//    public static int sumania(String a ){
-//        int result=0;
-//        for (int i = 0; i < a.length(); i++) {
-//            if(a.charAt(i)>47 && a.charAt(i)<58){
-//                result+=Integer.parseInt(String.valueOf(a.charAt(i)));
-//            }
-//
-//        }public class lab14 {
-//            //Write a method that can return the sum of the digits in a string
-//            public static void main(String[] args) {
-//                System.out.println(sumania("jg2fghh3h"));
-//            }
-//
-//
-//            public static int sumania(String a ){
-//                int result=0;
-//                for (int i = 0; i < a.length(); i++) {
-//                    if(a.charAt(i)>47 && a.charAt(i)<58){
-//                        result+=Integer.parseInt(String.valueOf(a.charAt(i)));
-//                    }
-//
-//                }
-//
-//                return result;
-//
-//            }
-//
-//
-//
-//        }
-//
-//        return result;
-//
-//    }
-//
-//
-//
-//}
-=======
+
 package student.Firat;
 
 public class lab14 {
@@ -56,43 +8,14 @@ public class lab14 {
     }
 
 
-    public static int sumania(String a ){
-        int result=0;
+    public static int sumania(String a) {
+        int result = 0;
         for (int i = 0; i < a.length(); i++) {
-            if(a.charAt(i)>47 && a.charAt(i)<58){
-                result+=Integer.parseInt(String.valueOf(a.charAt(i)));
+            if (a.charAt(i) > 47 && a.charAt(i) < 58) {
+                result += Integer.parseInt(String.valueOf(a.charAt(i)));
             }
 
         }
-        public class lab14 {
-            //Write a method that can return the sum of the digits in a string
-            public static void main(String[] args) {
-                System.out.println(sumania("jg2fghh3h"));
-            }
-
-
-            public static int sumania(String a ){
-                int result=0;
-                for (int i = 0; i < a.length(); i++) {
-                    if(a.charAt(i)>47 && a.charAt(i)<58){
-                        result+=Integer.parseInt(String.valueOf(a.charAt(i)));
-                    }
-
-                }
-
-                return result;
-
-            }
-
-
-
-        }
-
         return result;
-
     }
-
-
-
 }
->>>>>>> 17e6ad0f6072370bfd7fb3b52adecd24edb2520c
