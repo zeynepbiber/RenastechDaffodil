51ae414890039146c322d03eec97eb6fb5d22467
svgnhkmglu <sevginhekimoglu@gmail.com>
2022-05-18 09:54:03 -0500

    update

--- 03_SeleniumTestNG/src/test/java/code/day01_introduction/c1_HelloSelenium.java
+++ 03_SeleniumTestNG/src/test/java/code/day01_introduction/c1_HelloSelenium.java
@@ -6,38 +6,17 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 
 public class c1_HelloSelenium {
-
     public static void main(String[] args) {
         System.out.println("Hello World");
         //set up chrome driver
         WebDriverManager.chromedriver().setup();
 
-
-
-    public class c1_HelloSelenium {
-        public static void main(String[] args) {
-            System.out.println("Hello World");
-            //set up chrome driver
-            WebDriverManager.chromedriver().setup();
-
-            //create a driver instance
-            WebDriver driver = new ChromeDriver();
-
-            //navigate to chrome browser
-            driver.get("https://www.etsy.com/");
-
+        //create a driver instance
+        WebDriver driver = new ChromeDriver();
 
         //navigate to chrome browser
-         driver.get("https://www.etsy.com/");
+        driver.get("https://www.etsy.com/");
 
 
     }
-
-
-
-    }
-
 }
-
-
-
