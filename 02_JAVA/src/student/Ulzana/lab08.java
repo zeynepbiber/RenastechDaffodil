f68287eec213b7d047fc88b0e6d86febe1622dc9
Your Name <you@example.com>
2022-05-19 17:30:07 -0400

    update

--- 02_JAVA/src/student/Ulzana/lab08.java
+++ 02_JAVA/src/student/Ulzana/lab08.java
@@ -6,24 +6,29 @@ public class lab08 {
 
     public static void main(String[] args) {
 
-        int [] arr = {8, 10, 3, 7, 80,99};
-        System.out.println(Arrays.toString(numbers(arr)));
+        int [] numbers= {8, 3, 7,8,6};
+        System.out.println(Arrays.toString(ascendingNumbers(numbers)));
+
+
     }
 
     //(Array) Sort Ascending
     //Write a return method that can sort an int array in Ascending order without using the sort method of the Arrays class
     //Ex:    int[] arr = {10, 9, 8, 7};      ==>{ 7, 8, 9, 10};
 
-    public static int [] numbers(int [] numbers){
+    public static int [] ascendingNumbers(int [] numbers){
         int number = 0;
 
-        for (int i = 0; i < numbers.length; i++) {
+        for (int i = 0; i < numbers.length; i++) {    // arr[i]=8
+
             for (int j = 0; j < numbers.length; j++) {
+
                 if (numbers[i] < numbers[j]) {
 
                     number = numbers[i];
                     numbers[i] = numbers[j];
-                    numbers[j] = number;   } } }
+                    numbers[j] = number;
+                } } }
 
         return numbers;}
 
--- 02_JAVA/src/student/Ulzana/lab15.java
+++ 02_JAVA/src/student/Ulzana/lab15.java
@@ -11,7 +11,26 @@ public class lab15 {
    //  Input:  "DC501GCCCA098911"    OutPut: "CD015ACCCG011899"
 
         String name="DC501GCCCA098911";
+        String newString="";
+        String newString1="";
 
+        for (int i = 0; i <name.length() ; i++) {
+            if (name.charAt(i)>57){
+                newString +=""+name.charAt(i);
+                System.out.println("name = " + name.charAt(i));
+                if (name.charAt(i)<=47 && name.charAt(i)>=57 ){
+                    newString1+=""+name.charAt(i);
+                    System.out.println("name.charAt(i)IFFFF = " + name.charAt(i));
+
+                }
+
+
+            }
+
+
+        }
+        System.out.println(newString);
+        System.out.println(newString1);
 
     }
 
