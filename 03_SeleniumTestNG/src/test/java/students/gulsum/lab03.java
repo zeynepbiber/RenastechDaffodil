3cb4d37d990aa63325150e839622d65549c175ed
Your Name <gbulgan@hotmail.com>
2022-05-23 18:28:05 -0400

    renew

--- 03_SeleniumTestNG/src/test/java/students/gulsum/lab03.java
+++ 03_SeleniumTestNG/src/test/java/students/gulsum/lab03.java
@@ -80,6 +80,7 @@ public class lab03 {
         driver1.findElement(By.id("txtPassword")).sendKeys("admin123");
         WebElement loginbutton=driver1.findElement(By.name("Submit"));
         loginbutton.click();
+        System.out.println("test");
 
 
     }
