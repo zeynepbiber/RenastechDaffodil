9707ebd485344548d56e9be7b15d24b928fa40d6
haticekarakoyun <hatice.karakoyun9323@gmail.com>
2022-05-24 06:25:32 -0400

    update

---
+++ 03_SeleniumTestNG/src/test/java/students/HaticeKarakoyun/lab03.java
@@ -0,0 +1,33 @@
+package students.HaticeKarakoyun;
+
+public class lab03 {
+
+    //TASK: Google search
+//1- Open a Chrome browser
+//2- Go to: https://google.com
+//3- Write “apple” in search box
+//4- Click google search button
+//5- Verify title:
+//Expected: Title should start with "apple" word
+//6-navigate back
+//7-write cherry in search box
+//8-verify title contains cherry.
+
+
+
+//TASK2: Google Feeling lucky button
+//1- Open a Chrome browser
+//2- Go to: https://google.com
+//3- Write "apple" in search box
+//click i am feeling lucky button
+//Verify title contains Apple
+
+//go to https://opensource-demo.orangehrmlive.com/
+//locate username and send username, (username=Admin)
+//locate user password and send password (password=admin123)
+//click on the login button
+
+    public static void main(String[] args) {
+
+    }
+}
