05253a38cc7c77b42a0841b9188385ac3c29b256
haticekarakoyun <hatice.karakoyun9323@gmail.com>
2022-06-02 14:32:50 -0400

    pr fix Hatice Karakoyun

--- 02_JAVA/src/student/Firat/lab14.java
+++ 02_JAVA/src/student/Firat/lab14.java
@@ -1,52 +1,3 @@
-<<<<<<< HEAD
-package student.Firat;
-
-public class lab14 {
-    //Write a method that can return the sum of the digits in a string
-    public static void main(String[] args) {
-        System.out.println(sumania("jg2fghh3h"));
-    }
-
-
-    public static int sumania(String a ){
-        int result=0;
-        for (int i = 0; i < a.length(); i++) {
-            if(a.charAt(i)>47 && a.charAt(i)<58){
-                result+=Integer.parseInt(String.valueOf(a.charAt(i)));
-            }
-
-        }public class lab14 {
-            //Write a method that can return the sum of the digits in a string
-            public static void main(String[] args) {
-                System.out.println(sumania("jg2fghh3h"));
-            }
-
-
-            public static int sumania(String a ){
-                int result=0;
-                for (int i = 0; i < a.length(); i++) {
-                    if(a.charAt(i)>47 && a.charAt(i)<58){
-                        result+=Integer.parseInt(String.valueOf(a.charAt(i)));
-                    }
-
-                }
-
-                return result;
-
-            }
-
-
-
-        }
-
-        return result;
-
-    }
-
-
-
-}
-=======
 //
 //package student.Firat;
 //
@@ -68,4 +19,3 @@ public class lab14 {
 //        return result;
 //    }
 //}
->>>>>>> 7a8133640cbcfa07c209caa258722de35587f625
