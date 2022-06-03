537028fbb409bf0127270d16378efa1fa4bdcc35
desto <dbatgi01@gmail.com>
2022-06-02 22:42:34 -0400

    Lab01/02

---
+++ 03_SeleniumTestNG/src/test/java/students/Destyar/lab02Task2.java
@@ -0,0 +1,20 @@
+package students.Destyar;
+
+public class lab02Task2 {
+
+    //  Second Task:
+    // Go to https://www.etsy.com/
+    // Maximize window
+    // Verify title doesnt contains "Smile"
+    // Verify Current Url
+    // Go to https://www.amazon.com/
+    // Verify title
+    // Verify Current Url
+    // Navigate Back
+    // Refresh
+    // Quit browser
+
+    public static void main(String[] args) {
+
+    }
+}
