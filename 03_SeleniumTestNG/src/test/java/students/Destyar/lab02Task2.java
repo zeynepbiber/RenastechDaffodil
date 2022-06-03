2ce81b34df9e09ca66adc708683d38124f4c88d8
desto <dbatgi01@gmail.com>
2022-06-03 18:04:47 -0400

    lab02/2

--- 03_SeleniumTestNG/src/test/java/students/Destyar/lab02Task2.java
+++ 03_SeleniumTestNG/src/test/java/students/Destyar/lab02Task2.java
@@ -1,5 +1,10 @@
 package students.Destyar;
 
+import code.utilities.BrowserUtils;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class lab02Task2 {
 
     //  Second Task:
@@ -15,6 +20,35 @@ public class lab02Task2 {
     // Quit browser
 
     public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver chrome=new ChromeDriver();
+        // Maximize window
+        chrome.manage().window().maximize();
+        chrome.get("https://www.etsy.com/");
+        System.out.println("Current url : " +chrome.getCurrentUrl());
+
+        String expectedTitle="Smile";
+        String actualTitle=chrome.getTitle();
+
+        if (actualTitle.equals(expectedTitle)){
+            System.out.println("Verification has failed ");
+        }else{
+            System.out.println("Verification has passed");
+        }
+        BrowserUtils.wait(4);
+
+        chrome.manage().window().maximize();
+        chrome.get("https://www.amazon.com/");
+        System.out.println("Current url : " +chrome.getCurrentUrl());
+        System.out.println("Title : " +chrome.getTitle());
+
+        chrome.navigate().back();
+        chrome.navigate().refresh();
+        chrome.quit();
+
+
+
 
     }
 }
