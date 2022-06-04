4d21e649c3e3c1d69d270d922cf1fe1f9aafebfa
hasanraman <79028159+hasanraman@users.noreply.github.com>
2022-06-03 22:06:49 -0400

    day9

--- 03_SeleniumTestNG/src/test/java/code/day09_Webtables_windows/c1_WebTablePractice.java
+++ 03_SeleniumTestNG/src/test/java/code/day09_Webtables_windows/c1_WebTablePractice.java
@@ -5,6 +5,7 @@ import code.utilities.WebDriverUtil;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
@@ -24,5 +25,21 @@ public class c1_WebTablePractice {
     public void TC1_StreetVerification(){
         SmartBearUtils.loginForSmartBear(driver);
         Assert.assertEquals(driver.getTitle(),"Web Orders");
+    }
+
+    @Test(dataProvider ="TestDataforStreetName" )
+    public void TC2_StreetVerification2(String streetName){
+        SmartBearUtils.loginForSmartBear(driver);
+        SmartBearUtils.verifyStreet(driver,streetName);
+
+    }
+    @DataProvider(name="TestDataforStreetName")
+    public static Object[][]test(){
+        return new Object[][]{
+                {"17, Park Avenue"},
+                {"55 bordeaux-cartierville"},
+                {"44 Joseph-Napoléon-Henri Bourassa boulevard"},
+                {"21B Baker Street"}
+        };
     }
 }
