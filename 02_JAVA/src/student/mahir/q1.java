1f4c7ccb32b510272d78e76ccba8349099a2be68
mahiralkan <mahir.alkan.100@gmail.com>
2022-06-09 08:34:47 -0400

    Sanjay Package fix

--- 02_JAVA/src/student/mahir/q1.java
+++ 02_JAVA/src/student/mahir/q1.java
@@ -1,7 +1,27 @@
 package student.mahir;
 
+
+import java.util.*;
+
 public class q1 {
     public static void main(String[] args) {
+       Map<String ,Integer> maps=new HashMap<>();
+       maps.put("aa",56);
+       maps.put("bb",0);
+        maps.put("bb",90);
+        maps.put("bb",80);
+        maps.put("bb",-90);
+        maps.put("bb",5);
+
+        int min=maps.get("aa");
+        System.out.println(min);
+
+
+
+
 
     }
+
+
 }
+
