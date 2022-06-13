79f98d424f975944ea534a33f3f765e31670d190
mahiralkan <mahir.alkan.100@gmail.com>
2022-06-13 14:57:30 -0400

    pr attempt

--- 02_JAVA/src/student/mahir/q1.java
+++ 02_JAVA/src/student/mahir/q1.java
@@ -5,16 +5,7 @@ import java.util.*;
 
 public class q1 {
     public static void main(String[] args) {
-       Map<String ,Integer> maps=new HashMap<>();
-       maps.put("aa",56);
-       maps.put("bb",0);
-        maps.put("bb",90);
-        maps.put("bb",80);
-        maps.put("bb",-90);
-        maps.put("bb",5);
 
-        int min=maps.get("aa");
-        System.out.println(min);
 
 
 
