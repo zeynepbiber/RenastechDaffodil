822460dff1c7596d70fee5613f41b124506e601d
oralridvan <oral.ridvan@hotmail.com>
2022-06-21 20:10:58 -0400

    Cucumber day 04

--- 04_CucumberBDD/src/test/java/code/stepDefinitions/OrangeHRMSteps.java
+++ 04_CucumberBDD/src/test/java/code/stepDefinitions/OrangeHRMSteps.java
@@ -35,4 +35,42 @@ public class OrangeHRMSteps extends BrowserUtils {
     public void the_user_wants_to_verify_that_browser_landed_on(String string) {
        orangeHRMHome.verifyDashboardMessage(string);
     }
+
+    @Then("The user wants to click on PIM module")
+    public void the_user_wants_to_click_on_pim_module() {
+      orangeHRMHome.setPIM();
+    }
+    @Then("The user wants to go to Add Employee page")
+    public void the_user_wants_to_go_to_add_employee_page() {
+        orangeHRMHome.setAddEmployee();
+    }
+    @Then("The user wants to add first name as {string}")
+    public void the_user_wants_to_add_first_name_as(String string) {
+        orangeHRMHome.setFirstName(string);
+    }
+    @Then("The user wants to add last name as {string}")
+    public void the_user_wants_to_add_last_name_as(String string) {
+        orangeHRMHome.setLastName(string);
+    }
+    @Then("The user wants to save personal information")
+    public void the_user_wants_to_save_personal_information() {
+        orangeHRMHome.setSaveButton();
+    }
+    @Then("The user should be able to see {string}")
+    public void the_user_should_be_able_to_see(String string) {
+        orangeHRMHome.setPersonalDetailsHeader(string);
+    }
+
+    @Then("The user wants to add employee's first and last name as following")
+    public void the_user_wants_to_add_employee_s_first_and_last_name_as_following(io.cucumber.datatable.DataTable dataTable) {
+
+    }
+    @Then("The user wants to add login details as following")
+    public void the_user_wants_to_add_login_details_as_following(io.cucumber.datatable.DataTable dataTable) {
+
+    }
+    @Then("The user wants to save the information")
+    public void the_user_wants_to_save_the_information() {
+
+    }
 }
--- 04_CucumberBDD/src/test/resources/OrangeHRM.feature
+++ 04_CucumberBDD/src/test/resources/OrangeHRM.feature
@@ -13,3 +13,33 @@ Feature: Functional testing on Orange HRM
     When The user wants to enter username and password
     Then The user wants to click on Login button
     And The user wants to verify that browser landed on "Dashboard"
+
+  @Smoke @TC100
+  Scenario: Adding a new employee
+    Given The user wants to go to Orange HRM Website
+    When The user wants to enter username and password
+    Then The user wants to click on Login button
+    And The user wants to verify that browser landed on Dashboard
+    Then The user wants to click on PIM module
+    And The user wants to go to Add Employee page
+    Then The user wants to add first name as "Libby"
+    Then The user wants to add last name as "Renastech"
+    Then The user wants to save personal information
+    Then The user should be able to see "Personal Details"
+
+    @Smoke @TC200 @Regression
+    Scenario: Adding a new employee - different format
+      Given The user wants to go to Orange HRM Website
+      When The user wants to enter username and password
+      Then The user wants to click on Login button
+      And The user wants to verify that browser landed on Dashboard
+      Then The user wants to click on PIM module
+      Then The user wants to add employee's first and last name as following
+      |FirstName| Gulsen   |
+      |LastName | Renastech|
+      Then The user wants to add login details as following
+      |UserName|  gulsen147|
+      |Password|  123@gulsen|
+      |Status  | Disabled   |
+      Then The user wants to save the information
+      Then The user should be able to see "Personal Details"
